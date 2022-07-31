709889acd6c9cf6dfcd2a3786d10232db06240a9
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2022-07-31 12:00:04 +0200

    Pass an argument to a route
    In the  URL, the parameter is specified in angle brackets.
    A corresponding paramter must be used in the signature of the decorated
    function.

--- flasker/hello.py
+++ flasker/hello.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return '<h1>Hello world!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
