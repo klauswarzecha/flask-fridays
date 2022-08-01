006cf95553b8c3ef951f99a1fe670a94020b8463
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2022-08-01 08:00:21 +0200

    Pass argument to Jinja template
    The argument can be evaluate using a Jinja variable in double curly
    braces.

--- flasker/hello.py
+++ flasker/hello.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+@app.route('/user/<username>')
+def user(username):
+    return render_template('user.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
